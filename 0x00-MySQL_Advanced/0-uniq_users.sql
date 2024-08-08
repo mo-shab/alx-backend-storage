ff1ea83899c6dec098c1acaf2be82f7acd0f10dc
Mohammed <mrcloud.med@gmail.com>
2024-08-08 11:04:14 +0000

    Correcting auto

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -5,7 +5,7 @@
     -- If the table already exists, your script should not fail
 
 CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
